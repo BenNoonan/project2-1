58ba697a67831ac3686058b9fb2a34427d3c8cf4
Ben <btn9@txstate.edu>
2017-08-24 11:53:42 -0400

    Fixed variable post to List of post

--- imgScoop/src/main/java/com/imgscoop/scoops/Thread.java
+++ imgScoop/src/main/java/com/imgscoop/scoops/Thread.java
@@ -1,22 +1,33 @@
 package com.imgscoop.scoops;
 
+import java.util.List;
+
 public class Thread {
 
 	private int id;
 	private Tag tags;
 	private String title;
-	private Post posts;
+	private List<Post> posts;
 	
 	public Thread() {
 		super();
 	}
-	public Thread(int id, Tag tags, String title, Post posts) {
+	public Thread(int id, Tag tags, String title, List<Post> posts) {
 		super();
 		this.id = id;
 		this.tags = tags;
 		this.title = title;
 		this.posts = posts;
 	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
 	public int getId() {
 		return id;
 	}
@@ -35,16 +46,11 @@ public class Thread {
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public Post getPosts() {
-		return posts;
-	}
-	public void setPosts(Post posts) {
-		this.posts = posts;
-	}
 	@Override
 	public String toString() {
 		return "Thread [id=" + id + ", tags=" + tags + ", title=" + title + ", posts=" + posts + "]";
 	}
+	
 	
 	
 }
