21724e778acd36a229401a5bc6d0dec82400d35b
kurtcobian <cobian448@yahoo.com>
2017-08-28 11:31:19 -0400

    Changed name of UserDAOImpl and fixed PostDAO tests

--- imgScoop/src/main/java/com/imgscoop/dao/UserDAOImpl.java
+++ imgScoop/src/main/java/com/imgscoop/dao/UserDAOImpl.java
@@ -2,23 +2,31 @@ package com.imgscoop.dao;
 
 import java.util.List;
 
+import org.hibernate.SessionFactory;
+
 import com.imgscoop.scoops.User;
 
-public class UserDAOManager implements UserDAO {
+public class UserDAOImpl implements UserDAO {
+
+	private SessionFactory sessionFactory;
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
 
 	public void create(User user) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void update(User user) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void delete(User user) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public List<User> findByAll() {
