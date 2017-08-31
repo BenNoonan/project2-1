426d66a54105bcd9d175bea8bda1ac81bae9b6f4
kurtcobian <cobian448@yahoo.com>
2017-08-31 14:30:35 -0400

    pom fix and postController clean up

--- imgScoop/src/main/java/com/imgscoop/controllers/PostController.java
+++ imgScoop/src/main/java/com/imgscoop/controllers/PostController.java
@@ -1,27 +1,18 @@
 package com.imgscoop.controllers;
 
-import java.io.IOException;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import javax.servlet.http.HttpServletRequest;
 
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.imgscoop.scoops.Post;
-import com.imgscoop.scoops.User;
 import com.imgscoop.services.PostService;
 
 @Controller
