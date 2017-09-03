8ff327074853d2f616c9db8c8ab29328d2bc0a7c
Ben <btn9@txstate.edu>
2017-09-03 15:08:06 -0400

    Thread page Working

--- imgScoop/src/main/webapp/static/scripts/controller.js
+++ imgScoop/src/main/webapp/static/scripts/controller.js
@@ -17,26 +17,40 @@ angular.module('imgScoopApi')
 	console.log("I made it inside display all thread");
 	
 	$scope.count = 1;
-	$scope.pageNext = function() {
-		$scope.count++;
-	}
-	console.log($scope.count);
+	$scope.bool = 1;
 	$http({	
 		method: 'GET',
-		url: 'thread/page/1'//+$scope.count
+		url: 'thread/page/1'
 	})
 	.then(function(response) {
 		$scope.displayAllThread = response.data;
 	});
-});
-
-/*angular.module('imgScoopApi')
-.controller('pageCtrl', function($scope) {
-	console.log("Made pager");
-	
-	$scope.count = 1;
 	$scope.pageNext = function() {
 		$scope.count++;
+		$http({	
+			method: 'GET',
+			url: 'thread/page/'+$scope.count
+		})
+		.then(function(response) {
+			$scope.displayAllThread = response.data;
+			if($scope.displayAllThread.length < 15){
+				$scope.bool = -1;
+			}
+			console.log($scope.count);
+		});
 	}
-	console.log($scope.count);
-});*/
+	$scope.pagePrev = function() {
+		$scope.count--;
+		if($scope.count <= 1){
+			$scope.bool = 1;
+		}
+		$http({	
+			method: 'GET',
+			url: 'thread/page/'+$scope.count
+		})
+		.then(function(response) {
+			$scope.displayAllThread = response.data;
+		});
+	}
+	
+});
